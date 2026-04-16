db0d7fd55b3674b0544037922f3df3d93b2d2497
Gustavo Dupont <gustavo.dupont@gmail.com>
2026-04-16 11:08:37 -0700

    missing condiition

--- .cloudformation/reservoir.cfhighlander.rb
+++ .cloudformation/reservoir.cfhighlander.rb
@@ -11,6 +11,8 @@ CfhighlanderTemplate do
       ComponentParam 'Memory', '1024'
     end
 
+    Condition('DontSetDesireCount', FnEquals(Ref(:DesiredCount), '-1'))
+
     Component name: 'app', template: 'fargate-v2@0.7.5', render:Inline do
       parameter name: 'VPCId', value: FnImportValue(FnSub("${EnvironmentName}-vpc-VPCId"))
       parameter name: 'SubnetIds', value: FnSplit(',', FnImportValue(FnSub("${EnvironmentName}-vpc-ComputeSubnets")))
