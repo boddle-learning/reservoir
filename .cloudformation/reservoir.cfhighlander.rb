d3a0efd1e69b6366748e5584b28230574a238a63
Gustavo Dupont <gustavo.dupont@gmail.com>
2026-04-16 16:23:23 -0700

    setting internal alb

--- .cloudformation/reservoir.cfhighlander.rb
+++ .cloudformation/reservoir.cfhighlander.rb
@@ -13,13 +13,18 @@ CfhighlanderTemplate do
 
     Condition('DontSetDesireCount', FnEquals(Ref(:DesiredCount), '-1'))
 
+    # fargate-v2: when app.config.yaml uses a targetgroup *array*, the child stack expects
+    # one `{listener}Listener` parameter per target group (with rules), not `Listener` / `LoadBalancer`.
+    # Parent VPC/ALB stacks must export the internal ALB listener ARN and LB security group using
+    # the same names you pass to FnImportValue below (adjust Fn::Sub strings to match real exports).
     Component name: 'app', template: 'fargate-v2@0.7.5', render:Inline do
       parameter name: 'VPCId', value: FnImportValue(FnSub("${EnvironmentName}-vpc-VPCId"))
       parameter name: 'SubnetIds', value: FnSplit(',', FnImportValue(FnSub("${EnvironmentName}-vpc-ComputeSubnets")))
       parameter name: 'EcsCluster', value: FnImportValue(FnSub("${EnvironmentName}-ecs-EcsCluster"))
-      parameter name: 'LoadBalancer', value: FnImportValue(FnSub("${EnvironmentName}-alb-LoadBalancer"))
-      parameter name: 'Listener', value: FnImportValue(FnSub("${EnvironmentName}-alb-httpsListener"))
+      parameter name: 'httpsListener', value: FnImportValue(FnSub("${EnvironmentName}-alb-httpsListener"))
+      parameter name: 'internalListener', value: FnImportValue(FnSub("${EnvironmentName}-internalalb-httpsListener"))
       parameter name: 'LoadBalancerSecurityGroup', value: FnImportValue(FnSub("${EnvironmentName}-alb-SecurityGroupLoadBalancer"))
+      parameter name: 'InternalLoadBalancerSecurityGroup', value: FnImportValue(FnSub("${EnvironmentName}-internalalb-SecurityGroupLoadBalancer"))
       parameter name: 'DnsDomain', value: FnSub("${EnvironmentName}.env.boddlelearning.com")
       parameter name: 'DesiredCount', value: FnIf('DontSetDesireCount', Ref('AWS::NoValue'), Ref('DesiredCount'))
       parameter name: 'appScalingMin', value: Ref('MinCount')
